d210cc44122f0feff751a20b840a85ffeff629a4
xjayleex <aprilmagnificat@gmail.com>
2023-08-23 16:11:27 +0900

    added default config getter for shipper config

--- shipper/config/config.go
+++ shipper/config/config.go
@@ -1,9 +1,22 @@
 package config
 
 import (
+	"flag"
+	"time"
+
+	"github.com/xjayleex/minari-libs/queue/diskqueue"
+	"github.com/xjayleex/minari-libs/queue/memqueue"
 	"github.com/xjayleex/minari/shipper/output"
 	"github.com/xjayleex/minari/shipper/queue"
 )
+
+var (
+	cfgFilePath string
+)
+
+func init() {
+	flag.StringVar(&cfgFilePath, "c", "", "config file path for minari shipper")
+}
 
 type ShipperRootConfig struct {
 	Type    string        `config:"type"`
@@ -19,12 +32,49 @@ type ShipperConfig struct {
 }
 
 type ShipperConnectionConfig struct {
-	Server string `config:"server"`
-	// TLS ShipperTLS `config:"ssl"` //  `config:"tls"`
+	Server string     `config:"server"`
+	TLS    ShipperTLS `config:"tls"`
 }
 
-//type ShipperTLS struct {
-//	CAs []string `config:"certificate_authorities"`
-//	Cert string `config:"certificate"`
-//	Key string	`config:"Key"`
-//}
+type ShipperTLS struct {
+	CAs  []string `config:"certificate_authorities"`
+	Cert string   `config:"certificate"`
+	Key  string   `config:"Key"`
+}
+
+func FromConfigFile() (ShipperRootConfig, error) {
+	// TODO: impl unpack cfg from config file with resolved cfg file path.
+	return DefaultFromConfigFile()
+}
+
+func DefaultFromConfigFile() (ShipperRootConfig, error) {
+	return ShipperRootConfig{
+		Type: "default-config",
+		Shipper: ShipperConfig{
+			Queue: queue.Config{
+				MemSettings: &memqueue.Setting{
+					ACKListener:    nil,
+					Events:         256,
+					FlushMinEvents: 32,
+					FlushTimeout:   time.Second,
+					InputQueueSize: 0,
+				},
+				DiskConfig: &diskqueue.Config{},
+			},
+			Output: output.Config{
+				Console: &output.ConsoleConfig{
+					Enabled: true,
+				},
+			},
+			StrictMode: false,
+			ShipperConn: ShipperConnectionConfig{
+				Server: "unix:///tmp/minari_shipper_grpc.scoket",
+				TLS: ShipperTLS{
+					CAs:  []string{},
+					Cert: "",
+					Key:  "",
+				},
+			},
+		},
+	}, nil
+}
