f3d23b8142c12d84f216345c0c97fbcf0fca93e6
xjayleex <aprilmagnificat@gmail.com>
2023-08-24 09:45:03 +0900

    returning insecure creds when cfg.TLS.Strict not set

--- shipper/utils.go
+++ shipper/utils.go
@@ -2,8 +2,24 @@ package shipper
 
 import (
 	"net"
+
+	"github.com/xjayleex/minari/shipper/config"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func newUnixListener(addr string) (net.Listener, error) {
 	return net.Listen("unix", addr)
+}
+
+func Creds(c config.ShipperConnectionConfig) (credentials.TransportCredentials, error) {
+	if c.TLS.Cert == "" {
+		return insecure.NewCredentials(), nil
+	}
+	creds, err := credentials.NewServerTLSFromFile(c.TLS.Cert, c.TLS.Key)
+
+	if err != nil && !c.TLS.Strict {
+		return insecure.NewCredentials(), nil
+	}
+	return creds, err
 }
