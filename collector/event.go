3ca59c2bc2418a52fe8b771bb0be4954de594706
xjayleex <aprilmagnificat@gmail.com>
2023-08-29 16:45:01 +0900

    added abst methods for Batch interface

--- collector/event.go
+++ collector/event.go
@@ -6,6 +6,18 @@ import (
 
 type Batch interface {
 	Events() []datasource.Event
+	ACK()
+	Drop()
+	Retry()
+	RetryEvents(events []datasource.Event)
+	// Split this batch's events into two smaller batches and retry them both.
+	// If SplitRetry returns false, the batch could not be split, and the caller
+	// is reponsible for reporting the error(including calling batch.Drop() if
+	// necessary)
+	SplitRetry() bool
+	FreeEntries()
+	// Send was aborted, try again but don't decrease the batch's TTL counter.
+	Cancelled()
 }
 
 type batch []datasource.Event
