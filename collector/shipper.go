13e6066a04de0a33e35bd07c9dc31d1cb07b2cf5
xjayleex <aprilmagnificat@gmail.com>
2023-08-30 17:10:01 +0900

    mod publish method

--- collector/shipper.go
+++ collector/shipper.go
@@ -3,14 +3,19 @@ package collector
 import (
 	"context"
 	"errors"
+	"fmt"
+	"sync"
 	"time"
 
 	pb "github.com/xjayleex/minari-libs/api/proto/grpc"
 	"github.com/xjayleex/minari-libs/api/proto/messages"
+	"github.com/xjayleex/minari-libs/logpack"
 	"github.com/xjayleex/minari/collector/datasource"
 	server "github.com/xjayleex/minari/shipper"
 	shipperconfig "github.com/xjayleex/minari/shipper/config"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -20,13 +25,33 @@ const (
 type shipper struct {
 	cfg shipperconfig.ShipperRootConfig
 
-	conn   *grpc.ClientConn
-	client pb.ProducerClient
-
+	logger   logpack.Logger
 	observer Observer
+
+	uuid string
+
+	conn      *grpc.ClientConn
+	client    pb.ProducerClient
+	ackClient pb.Producer_PersistedIndexClient
+
+	// The publish function sends to ackLoopChan to notify the ack worker of
+	// new pending batches
+	ackBatchChan chan pendingBatch
+
+	// The ack RPC listener sends to ackIndexChan to notify the ack worker
+	// of the new persisted index
+	ackIndexChan chan uint64
+
+	// ackWaitGroup is used to synchronize the shutdown of the ack listener
+	// and the ack worker when a connection is closed.
+	ackWaitGroup sync.WaitGroup
+
+	// ackCancel cancels the context for the ack listener and the ack worker,
+	// notifying them to shut down.
+	ackCancel context.CancelFunc
 }
 
-var transfomer = toShipperEvent
+var converter = toShipperEvent
 
 func (s *shipper) Connect() error {
 	creds, err := server.Creds(s.cfg.Shipper.ShipperConn)
@@ -56,6 +81,7 @@ func (s *shipper) Connect() error {
 
 	s.client = pb.NewProducerClient(s.conn)
 
+	//FIXME:
 	return nil
 }
 
@@ -77,23 +103,96 @@ func (s *shipper) publish(ctx context.Context, batch Batch) error {
 
 	toSend := make([]*messages.Event, 0, len(events))
 
-	var dropped int = 0
+	var droppedCount int = 0
 
-	for _, e := range events {
-		converted, err := transfomer(e)
+	for i, e := range events {
+		converted, err := converter(e)
 		if err != nil {
-			//TODO: log
-			dropped += 1
+			// conversion errors are not recoverable, so we have to drop the event completely
+			s.logger.Errorf("%d/%d: %q, dropped", i+1, len(events), err)
+
+			droppedCount += 1
 			continue
 		}
 		toSend = append(toSend, converted)
 	}
 
-	return errors.New("TODO:")
+	convertedCount := len(toSend)
+
+	s.observer.Dropped(droppedCount)
+
+	var lastAcceptedIndex uint64
+
+	ctx, cancel := context.WithTimeout(ctx, s.cfg.Client.Timeout)
+	defer cancel()
+
+	for len(toSend) > 0 {
+		reply, err := s.client.PublishEvents(ctx, &messages.PublishRequest{
+			Uuid:   s.uuid,
+			Events: toSend,
+		})
+
+		if err != nil {
+			if status.Code(err) == codes.ResourceExhausted {
+				if batch.SplitRetry() {
+					s.observer.Split()
+				} else {
+					batch.Drop()
+					s.observer.Dropped(len(events))
+				}
+				return nil
+			}
+			batch.Cancelled()
+			s.observer.Cancelled(len(events))
+			return fmt.Errorf("failed to publish the batch to the shipper, none of the %d events were accepted: %w", len(toSend), err)
+		}
+
+		if int(reply.AcceptedCount) > len(toSend) {
+			return fmt.Errorf(
+				"server returned unexpected results, expected maximum accepted items %d, got %d",
+				len(toSend),
+				reply.AcceptedCount,
+			)
+		}
+
+		toSend = toSend[reply.AcceptedCount:]
+		lastAcceptedIndex = reply.AcceptedIndex
+	}
+
+	s.logger.Debugf("total of %d events have been accepted from batch, %d dropped", convertedCount, droppedCount)
+
+	batch.FreeEntries()
+	s.ackBatchChan <- pendingBatch{
+		batch:        batch,
+		index:        lastAcceptedIndex,
+		eventCount:   len(events),
+		droppedCount: droppedCount,
+	}
+
+	return nil
 }
 
-func (s *shipper) Close() {}
+func (s *shipper) Close() error {
+	if s.conn == nil {
+		return fmt.Errorf("connection is not established yet")
+	}
+
+	s.ackCancel()
+	s.ackWaitGroup.Wait()
+
+	err := s.conn.Close()
+	s.conn = nil
+	s.client = nil
+	return err
+}
 
 func toShipperEvent(e datasource.Event) (*messages.Event, error) {
 	return nil, errors.New("TODO:")
+}
+
+type pendingBatch struct {
+	batch        Batch
+	index        uint64
+	eventCount   int
+	droppedCount int
 }
