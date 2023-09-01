fa407958e67b79a3a1dd763c494267a071ac8322
xjayleex <aprilmagnificat@gmail.com>
2023-09-01 10:09:43 +0900

    added Meta field to Event struct

--- collector/datasource/event.go
+++ collector/datasource/event.go
@@ -4,10 +4,12 @@ import (
 	"time"
 
 	"github.com/xjayleex/minari-libs/api/proto/messages"
+	"github.com/xjayleex/minari-libs/thirdparty/mapstr"
 )
 
 type Event struct {
 	Timestamp  time.Time
+	Meta       mapstr.M
 	Private    interface{}
 	TimeSeries bool
 	TypedEvent struct {
